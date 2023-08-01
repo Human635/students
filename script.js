4d20ae42d26799d30237d2454a0e8b23ba315942
Xojiakbar <139034828+Human635@users.noreply.github.com>
2023-08-01 13:57:38 +0500

    НУМЕРАЦИЯ

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 //inputs
 let studentName = document.querySelector("#stName")
 let studentSurname = document.querySelector("#stSurname")
-let studentNumber = document.querySelector("#stNumber")
 
 let addStudentButton = document.querySelector(".add-st")
 let studentListElement = document.querySelector(".students-list")
@@ -9,58 +8,46 @@ let studentListElement = document.querySelector(".students-list")
 let studentsArray = []
 let stId = 0
 
-function validPhone() {
-    let check = /^[\d\+]{3}\ \([\d]{2}\)\ [\d]{3}-[\d]{2}-[\d]{2}$/;
-    let stPhone = document.getElementById('#stNumber').value;
-    let valid = check.test(stPhone);
-    if (valid) output = 'PHONE NUMBER - CORRECT!';
-    else output = 'PHONE NUMBER - INVALID!';
-    return valid;
-}
-
 addStudentButton.addEventListener("click", addStudent)
 
-    function addStudent() {
+function addStudent() {
     let studentNameValue = studentName.value
     let studentSurnameValue = studentSurname.value
-    let studentNumberValue = studentNumber.value
 
     stId += 1
 
     let studentId = stId
         
     let studentObj = {
-        studentId: stId,
+        studentId: studentId,
         studentNameKey: studentNameValue,
         studentSurnameKey: studentSurnameValue,
-        studentNumberKey: studentNumberValue
     }
 
     studentsArray.push(studentObj)
-
-    console.log(studentsArray)
     rerenderStudentsTable()
 }
 
 function deleteStudent(studentId) {
-    //Через фильтр
-    studentsArray.filter(student => student.studentIdId !== studentIdroductId)
+    //Через splice
+    let studentIdx = studentsArray.findIndex(student => student.studentId === studentId)
+    studentsArray.splice(studentIdx, 1)
+    rerenderStudentsTable()
 }
 
 function rerenderStudentsTable(){
     let innerHtmlWithStudents = ''
-}
 
-    let innerHtmlWithStudents = ""
-    
-    studentsArray.forEach((item) => {
-        innerHtmlWithStudents += `<div class='product'>
+studentsArray.forEach((item, index) => {
+    innerHtmlWithStudents += `<div class='product'>
         <div class='description-wrapper'>
+            <p>NUMBER: ${index + 1}</p>
             <p>NAME: ${item.studentNameKey}</p>
             <p>SURNAME: ${item.studentSurnameKey}</p>
-            <p>Phone Number: ${item.studentNumberKey}</p>
+            <button class="remove-button" onclick="deleteStudent(${item.studentId})">Delete</button>
         </div>
-        </div>`
-    });    
+    </div>`
+});
     
     studentListElement.innerHTML = innerHtmlWithStudents
+}
